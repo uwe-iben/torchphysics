11e558c68dc899e2d17c7cdc3ee6d560bd39f4f3
Nick Heilenkötter <nick7@uni-bremen.de>
2021-09-27 22:29:10 +0200

    initial version of lambdadomain
    Signed-off-by: Nick Heilenkötter <nick7@uni-bremen.de>

--- src/torchphysics/problem/domain/lambdadomain.py
+++ src/torchphysics/problem/domain/lambdadomain.py
@@ -1,28 +1,66 @@
 from .domain import Domain
+from ...utils.user_fun import UserFunction
+import inspect
+import functools
 
 
 class LambdaDomain(Domain):
-    def __init__(self, class_, params, space, dim):
+    def __init__(self, constructor, params, space, dim, necessary_variables=None):
+        """
+        params : A dictionary containing all the params needed to create the given
+            domain_class domain
+        """
         super().__init__(space, dim=dim)
-        self.class_ = class_
+        self.constructor = constructor
         self.params = params
 
+        # create a set of variables/spaces that this domain needs to be properly defined
+        if necessary_variables is not None:
+            self.necessary_variables = necessary_variables
+        else:
+            self.necessary_variables = set()
+            for key in params:
+                if callable(params[key]):
+                    if '*' in str(inspect.signature(params[key])):
+                        raise ValueError("""Functions in Domain definitions should use proper keys,
+                                            as defined in their spaces.""")
+                    params[key] = UserFunction(params[key])
+                    for k in params[key].necessary_args:
+                        self.necessary_variables.add(k)
+
     def __call__(self, **data):
-        for variable in data:
-            
-        p = {}
-        for k in self.params:
-            if callable(self.params[k]):
-                p[k] = self.params[k](data)
+        """
+        Slice the domain along given axis, i.e. for given (partial) data
+        """
+
+    def _call_param(param, args):
+        if callable(param):
+            if all(arg in args for arg in param.necessary_args):
+                return param(**args)
             else:
-                p[k] = self.params[k]
-        return self.class_(space=self.space, **p)
+                param.set_default(**args)
+        return param
 
     def __mul__(self, other):
-        return LambdaProductDomain(self, other)
+        if not isinstance(other, LambdaDomain) and all(var in other.space for var in self.necessary_variables):
+            return LambdaProductDomain(self, other)
+        if isinstance(other, LambdaDomain):
+            return LambdaDomain
+
+    def _get_necessary_args(fun):
+        """
+        Returns the (positional or keyword-)arguments of fun which don't supply
+        default values
+        NOTE: we need a whole library utils part for user-function handling
+        """
+
 
 
 class LambdaProductDomain(ProductDomain):
+    def __init__(self, other):
+        super().__init__()
+        if isinstance(self, LambdaDomain):
+
     def is_inside(self, points):
         return super().is_inside(points)
 
---
+++ src/torchphysics/utils/user_fun.py
@@ -0,0 +1,78 @@
+import inspect
+import numpy as np
+
+class UserFunction:
+    def __init__(self, fun):
+        self.fun = fun
+
+        f_args = inspect.getfullargspec(self.fun).args
+
+        # we check that the function defines all needed parameters
+        if inspect.getfullargspec(self.fun).varargs is not None or \
+            inspect.getfullargspec(self.fun).varkw is not None:
+            raise ValueError("""
+                             Variable arguments are not supported in
+                             UserFunctions. Please use keyword arguments.
+                             """)
+
+        f_defaults = inspect.getfullargspec(self.fun).defaults
+        f_kwonlyargs = inspect.getfullargspec(self.fun).kwonlyargs
+        f_kwonlydefaults = inspect.getfullargspec(self.fun).kwonlydefaults
+
+        # NOTE: By above check, there should not be kwonlyargs. However, we still catch
+        # this case here.
+        self.args = f_args + f_kwonlyargs
+        # defaults always align at the end
+        self.defaults = {self.f_args[-i]: f_defaults[-i] for i in range(len(f_defaults), 0, -1)}
+        self.defaults.update(f_kwonlydefaults)
+
+    def __call__(self,  vectorize=False, **args):
+        # check that every necessary arg is given
+        for key in self.necessary_args:
+            assert key in args, \
+                f"The argument '{key}' is necessary in {self.__name__} but not given."
+        # if necessary, pass defaults
+        inp = {key: args[key] for key in self.args if key in args}
+        inp.update({key: self.defaults[key] for key in self.args if key not in args})
+        if not vectorize:
+            return self.fun(**inp)
+        else:
+            return self.apply_to_batch(inp)
+
+    def apply_to_batch(self, inp):
+        # apply the function to a batch of elements by running a for-loop
+
+        # we assume that all inputs either have batch (i.e. maximum) dimension or
+        # are a constant param
+        batch_size = max(len(inp[key]) for key in inp)
+        out = []
+        for i in range(batch_size):
+            inp_i = {}
+            for key in inp:
+                if len(inp[key]) == batch_size:
+                    inp_i[key] = inp[key][i]
+                else:
+                    inp_i[key] = inp[key]
+            o = self.fun(**inp_i)
+            if o is not None:
+                out.append(o)
+
+    def __name__(self):
+        return self.func.__name__
+
+    def set_default(self, **args):
+        self.defaults.update({key: args[key] for key in args if key in self.args})
+
+    def remove_default(self, *args, **kwargs):
+        for key in args:
+            self.defaults.pop(key)
+        for key in kwargs:
+            self.defaults.pop(key)
+
+    @property
+    def necessary_args(self):
+        return [arg for arg in self.args if arg not in self.defaults]
+
+    @property
+    def optional_args(self):
+        return [arg for arg in self.args if arg in self.defaults]
